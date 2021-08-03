7dbc28465cfef6bacd0d13affa8b2b59d988e5f0
Raluca Perde <raluca.perde98@gmail.com>
2021-08-03 19:25:39 +0300

    Add delete implementation
    Add delete functionality in dashboard,
    update dashboard.

---
+++ src/main/java/ro/siit/service/FlightService.java
@@ -0,0 +1,22 @@
+package ro.siit.service;
+
+import ro.siit.model.FlightDto;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface FlightService {
+
+    Optional<FlightDto> findById(Long id);
+
+    List<FlightDto> findAllFlights();
+
+    List<FlightDto> findAllActualFlights();
+
+    boolean addFlight(FlightDto flightDto);
+
+    void updateFlight(Long id, FlightDto flightDto);
+
+    boolean deleteFlight(Long id);
+}
+
